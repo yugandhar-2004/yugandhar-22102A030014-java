dd0d48f7b2073483cca808e7e83cd95c13fd27c1
yugandhar-2004 <yugandhar9704887327@gmail.com>
2025-06-24 14:06:32 +0530

    Add files via upload

---
+++ sumofelements.java
@@ -0,0 +1,13 @@
+public class sumofelements {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+        int sum=0;
+        for(int i=0;i<5;i++)
+        {
+            sum+=arr[i];
+        }
+        System.out.println("sum:"+sum);
+        System.out.println("avg:"+sum/5);
+    }
+    
+}
